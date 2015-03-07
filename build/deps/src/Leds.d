8e53e908665263f6ee31cb4d3ffe34f1adb56752
Mateusz Szynka <mateusz.szynka@gmail.com>
2015-03-07 13:56:56 +0100

    .gitignore fix

--- build/deps/src/Leds.d
+++
@@ -1,8 +0,0 @@
-build/objs/src/Leds.o: src/Leds.cpp include/Leds.hpp \
- lib/include/stm32f4xx.h lib/include/stm32f407xx.h lib/include/core_cm4.h \
- lib/include/core_cmInstr.h lib/include/core_cmFunc.h \
- lib/include/core_cmSimd.h include/system_stm32f4xx.h \
- lib/include/stm32f4xx_hal_gpio.h lib/include/stm32f4xx_hal_def.h \
- lib/include/stm32f4xx.h lib/include/Legacy/stm32_hal_legacy.h \
- lib/include/stm32f4xx_hal_gpio_ex.h lib/include/stm32f4xx_hal_rcc.h \
- lib/include/stm32f4xx_hal_rcc_ex.h
--- build/deps/src/stm32f4xx_it.d
+++
@@ -1,2 +0,0 @@
-build/objs/src/stm32f4xx_it.o: src/stm32f4xx_it.c \
- lib/include/stm32f4xx_it.h
