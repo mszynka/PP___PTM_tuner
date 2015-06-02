646a08a6610dd731026310fd52209156e54497c6
Mateusz Szynka <devmsw12@gmail.com>
2015-06-02 09:56:12 +0200

    Game project - rewrite init

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_spi.h"
+
+#include "tm_stm32f4_pcd8544.h"
+
+int main(void)
+{
+	// 1. accelerometer
+	// 2. leds accel output
+	// 3. button GPIO
+	// 4. game project
+
+    while(1)
+    {
+    }
+}
